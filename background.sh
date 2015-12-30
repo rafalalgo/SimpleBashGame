a21fcc7994d5a7895a58c1aaefe19fa2df625622
Rafał Byczek <rafal.algo@gmail.com>
2015-12-31 00:08:31 +0100

    Lepsze tlo

--- background.sh
+++ background.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+printf_ekran()
+{
 ###################################################
 #     ekran przed gra
 ###################################################
+
 tput civis
 tput bold
 clear
 tput setaf 4
 echo -e "\n\n"
-echo "       OOO       OOO      OOOOO       OOOOOOO     OOOOOOO     OOOOO"
-echo "       OOOO     OOOO     OOOOOOO     OOOOOOOOO    OOOOOOO    OOOOOOO"
+echo "       OOO       OOO      OOOOO       OOOOOOOO    OOOOOOO     OOOOO"
+echo "       OOOO     OOOO     OOOOOOO     OOOOOOOOOO   OOOOOOO    OOOOOOO"
 echo "       OOOOO   OOOOO    OOO   OOO    OOO    OOO     OOO     OOO   OOO"
 echo "       OOOOOO OOOOOO   OOO     OOO   OOO    OOO     OOO    OOO     OOO"
 echo "       OOO OOOOO OOO   OOOOOOOOOOO   OOOOOOOOOO     OOO    OOO     OOO"
@@ -24,6 +27,7 @@ read zmienna
 ###################################################
 #     wstepny zarys planszy
 ###################################################
+tput setab 4
 tput setaf 4
 K=80;
 M=16;
@@ -37,36 +41,37 @@ touch plansza_wy.in
 
 for((i=1;i<=$M;i++))
 do
-	echo -n "@$i@ " >> plansza_we.in
-	for((j=1;j<=$K;j++))
-	do
-		echo -n "%$j%=" >> plansza_we.in
-	done
-	echo -e "\n" >> plansza_we.in	
+echo -n "@$i@" >> plansza_we.in
+for((j=1;j<=$K;j++))
+do
+	echo -ne "%$j%s" >> plansza_we.in
+done
+echo -e "\n" >> plansza_we.in	
 done
 
 for((i=$[M+1];i<=$W;i++))
 do
-	echo -n "@$i@ " >> plansza_we.in
-	for((j=1;j<=$K;j++))
-	do
-		echo -n "%$j%^" >> plansza_we.in
-	done
-	echo -e "\n" >> plansza_we.in	
+echo -n "@$i@" >> plansza_we.in
+for((j=1;j<=$K;j++))
+do
+	echo -n "%$j%^" >> plansza_we.in
+done
+echo -e "\n" >> plansza_we.in	
 done
 
 clear
-sed 's/[0-9 @%\n]//g' plansza_we.in > plansza_wy.in
+sed 's/[0-9@%\n]//g' plansza_we.in > plansza_wy.in
 I=1;
 for WERS in $(cat plansza_wy.in)
 do
-	if [ $I -gt 16 ];
-	then
-		tput setaf 2
-	fi
-	
-	echo "$WERS"
-	I=$[I+1];
+if [ $I -gt 16 ];
+then
+	tput setab 2
+	tput setaf 2
+fi
+
+echo "$WERS"
+I=$[I+1];
 done
 
 t=1;
@@ -77,15 +82,16 @@ t=1;
 
 for((h=1;h<=3;h++))
 do
-	for((i=10;i<=14;i++))
+for((i=10;i<=14;i++))
+do
+	for((j=$[h*20+9];j<=$[h*20+11];j++))
 	do
-		for((j=$[h*20+9];j<=$[h*20+11];j++))
-		do
-			tput setaf 0
-			tput cup $i $j
-			echo -n "|"
-		done
+		tput setab 0
+		tput setaf 0
+		tput cup $i $j
+		echo -n " "
 	done
+done
 done
 
 ###################################################
@@ -94,29 +100,32 @@ done
 
 for((h=1;h<=3;h++))
 do
-	for((j=$[h*20+7];j<=$[h*20+13];j++))
+for((j=$[h*20+7];j<=$[h*20+13];j++))
+do
+	tput setab 2
+	tput setaf 2
+	tput cup 4 $j
+	echo -n "*"
+done
+
+for((j=$[h*20+7];j<=$[h*20+13];j++))
+do
+	tput setab 2
+	tput setaf 2
+	tput cup 9 $j
+	echo -n "*"
+done
+
+for((i=8;i>=5;i--))
+do
+	for((j=$[h*20+6];j<=$[h*20+14];j++))
 	do
+		tput setab 2
 		tput setaf 2
-		tput cup 4 $j
-		echo -n "$"
+		tput cup $i $j
+		echo -n "*"
 	done
-	
-	for((j=$[h*20+7];j<=$[h*20+13];j++))
-	do
-		tput setaf 2
-		tput cup 9 $j
-		echo -n "$"
-	done
-	
-	for((i=8;i>=5;i--))
-	do
-		for((j=$[h*20+6];j<=$[h*20+14];j++))
-		do
-			tput setaf 2
-			tput cup $i $j
-			echo -n "$"
-		done
-	done
+done
 done
 
 ###################################################
@@ -125,30 +134,35 @@ done
 
 for((i=3;i<=4;i++))
 do
-	for((j=$[4-i+1];j<$[4+i-1];j++))
-	do
-		tput setaf 3
-		tput cup $[i - 2] $j
-		echo -n "O"
-	done
-done
-i=3;
 for((j=$[4-i+1];j<$[4+i-1];j++))
 do
+	tput setab 3
 	tput setaf 3
-	tput cup $i $j
-	echo -n "O"
+	tput cup $[i - 2] $[j+1]
+	echo -n "o"
+done
 done
 
+for((j=$[4-3+1];j<$[4+3-1];j++))
+do
+tput setab 3
+tput setaf 3
+tput cup 3 $[j+1]
+echo -n "o"
+done
+}
 ###################################################
 #     tu cos sie musi zaczac dziac
 ###################################################
 
+printf_ekran
+
 while [ $t == 1 ];
 do
+	tput setab 3
 	tput setaf 3
 	tput cup 2 3
-	echo "O"
+	echo "o"
 done
 
 #Rafał Byczek
