4f848185232ba0f6b348e71088d814b5e3b1db09
Dawid Raczka <dawidraczka@gmail.com>
2015-12-31 21:11:58 +0000

    background.sh edited online with Bitbucket

--- background.sh
+++ background.sh
@@ -5,80 +5,173 @@
 #	
 #}
 
-#rysuj_postac_kucajaca()
-#{
-#	
-#}
+###################################################
+#     postac kucajaca
+###################################################
+rysuj_postac_kucajaca()
+{
+	# GŁOWA
+	tput setab 7
+	tput setaf 7
+	tput cup 13 9; echo "#"
+	tput setab 3
+	tput setaf 3
+	tput cup 13 10; echo " "
+	tput setab 7
+	tput setaf 7
+	tput cup 13 11; echo "#"
+	tput setab 3
+	tput setaf 3
+	tput cup 14 8; echo " "
+	tput setab 9
+	tput setaf 9
+	tput cup 14 9; echo "*"
+	tput setab 3
+	tput setaf 3
+	tput cup 14 10; echo " "
+	tput setab 9
+	tput setaf 9
+	tput cup 14 11; echo "*"
+	tput setab 3
+	tput setaf 3
+	tput cup 14 12; echo " "
+	tput cup 15 9; echo "[-]"
+	tput setab 1
+	tput setaf 1
+	tput cup 16 9; echo ">-<"
+	#KLATKA PIERSIOWA
+	tput setab 9
+	tput setaf 9
+	tput cup 17 7;  echo "   "
+	tput setab 7
+	tput setaf 7
+	tput cup 17 10;  echo " "
+	tput setab 9
+	tput setaf 9
+	tput cup 17 11;  echo "  "
+	tput cup 18 6;  echo "   "
+	tput setab 3
+	tput setaf 3
+	tput cup 18 9;  echo "="
+	tput setab 7
+	tput setaf 7
+	tput cup 18 10;  echo " "
+	tput setab 9
+	tput setaf 9
+	tput cup 18 11;  echo "   "
+	tput setab 3
+	tput setaf 3
+	tput cup 18 14;  echo "="
+	tput setab 9
+	tput setaf 9
+	tput cup 19 8;  echo "  "
+	tput setab 7
+	tput setaf 7
+	tput cup 19 10;  echo " "
+	tput setab 9
+	tput setaf 9
+	tput cup 19 11;  echo "  "
+	tput setab 3
+	tput setab 7
+	tput setaf 7
+	tput cup 20 7;  echo " "
+	tput setab 9
+	tput setaf 9
+	tput cup 20 8;  echo "     "
+	tput setab 7
+	tput setaf 7
+	tput cup 21 7;  echo " "
+	tput setab 9
+	tput setaf 9
+	tput cup 21 8;  echo "  "
+	tput setab 7
+	tput setaf 7
+	tput cup 21 11;  echo "   "
+}
 
+###################################################
+#     postac stojaca
+###################################################
 rysuj_postac_stojaca()
 {
+# GŁOWA
+tput setab 7
+tput setaf 7
+tput cup 11 9; echo "#"
 tput setab 3
 tput setaf 3
-tput cup 11 9; echo "   "
-tput cup 12 8; echo "     "
-tput cup 13 9; echo "   "
-tput cup 14 10; echo " "
-tput setab 1
-tput setaf 1
-tput cup 15 6;  echo "         "
-tput cup 16 6;  echo -n " "
-tput setab 2
-tput setaf 2
-echo -n "^"
-tput setab 1
-tput setaf 1
-echo -n "     "
-tput setab 2
-tput setaf 2
-echo -n "^"
-tput setab 1
-tput setaf 1
-echo -n " "
-tput cup 17 6;  
-echo -n " "
-tput setab 2
-tput setaf 2
-echo -n "^"
-tput setab 1
-tput setaf 1
-echo -n "     "
-tput setab 2
-tput setaf 2
-echo -n "^"
-tput setab 1
-tput setaf 1
-echo -n " "
-tput cup 18 6;  
+tput cup 11 10; echo " "
+tput setab 7
+tput setaf 7
+tput cup 11 11; echo "#"
 tput setab 3
 tput setaf 3
-echo -n " "
-tput setab 2
-tput setaf 2
-echo -n "^"
-tput setab 1
-tput setaf 1
-echo -n "     "
-tput setab 2
-tput setaf 2
-echo -n "^"
+tput cup 12 8; echo " "
+tput setab 9
+tput setaf 9
+tput cup 12 9; echo "*"
 tput setab 3
 tput setaf 3
-echo -n " "
+tput cup 12 10; echo " "
+tput setab 9
+tput setaf 9
+tput cup 12 11; echo "*"
+tput setab 3
+tput setaf 3
+tput cup 12 12; echo " "
+tput cup 13 9; echo "[-]"
 tput setab 1
 tput setaf 1
+tput cup 14 9; echo ">-<"
+#KLATKA PIERSIOWA
+tput setab 9
+tput setaf 9
+tput cup 15 7;  echo "   "
+tput setab 7
+tput setaf 7
+tput cup 15 10;  echo " "
+tput setab 9
+tput setaf 9
+tput cup 15 11;  echo "   "
+tput cup 16 6;  echo "    "
+tput setab 7
+tput setaf 7
+tput cup 16 10;  echo " "
+tput setab 9
+tput setaf 9
+tput cup 16 11;  echo "    "
+tput cup 17 6;  echo "    "
+tput setab 7
+tput setaf 7
+tput cup 17 10;  echo " "
+tput setab 9
+tput setaf 9
+tput cup 17 11;  echo "    "
+tput setab 3
+tput setaf 3
+tput cup 18 6; echo "||"
+tput setab 9
+tput setaf 9
+tput cup 18 8;  echo "     "
+tput setab 3
+tput setaf 3
+tput cup 18 13; echo "||"
+#SPODNIE
+tput setab 9
+tput setaf 9
 tput cup 19 8;  echo -n "  "
 tput setab 2
 tput setaf 2
 echo -n "^"
-tput setab 1
-tput setaf 1
+tput setab 9
+tput setaf 9
 echo -n "  " 
 tput cup 20 8;  echo -n "  "
 tput setab 2
 tput setaf 2
 echo -n "^"
-tput setab 1
-tput setaf 1
+tput setab 9
+tput setaf 9
 echo -n "  " 
 tput setab 7
 tput setaf 7
@@ -136,7 +229,11 @@ echo "       OOO  OOO  OOO   OOOOOOOOOOO   OOOOOOOO       OOO    OOO     OOO "
 echo "       OOO   O   OOO   OOO     OOO   OOO  OOO       OOO     OOO   OOO"
 echo "       OOO       OOO   OOO     OOO   OOO   OOO    OOOOOOO    OOOOOOO"
 echo "       OOO       OOO   OOO     OOO   OOO    OOO   OOOOOOO     OOOOO"
-echo -e "\n\n\n\n\n"
+echo -e "\n\n"
+tput setaf 3
+echo "      			      JKM SPECIAL EDITION"
+tput setaf 4
+echo -e "\n\n"
 echo "              Nacisnij dowolny klawisz zeby rozpoczac rozgrywke."
 read zmienna
 
@@ -277,7 +374,8 @@ stan=1
 min=11
 max=21
 
-rysuj_postac_stojaca
+#rysuj_postac_stojaca
+rysuj_postac_kucajaca
 
 while [ $t -eq 1 ] || [ $t -eq 2 ] || [ $t -eq 3 ];
 do
@@ -316,14 +414,14 @@ do
 	if [ $t -eq 3 ] && [ $stan -eq 1 ];
 	then
 		wyczysc_dol_planszy
-		#rysuj_postac_kucajaca
+		rysuj_postac_kucajaca
 		stan=3
 	fi
 	
 	if [ $t -eq 3 ] && [ $stan -eq 2 ];
 	then
 		wyczysc_dol_planszy
-		#rysuj_postac_kucajaca
+		rysuj_postac_kucajaca
 		stan=3
 	fi
 	
@@ -334,3 +432,4 @@ do
 done
 
 #Rafał Byczek
+#Dawid Rączka
